0cffdd664ecf33d0e2e08a5c8ae76e435048e133
PaulinaArvizu <is707571@iteso.mx>
2020-04-13 19:43:09 -0500

    Navegación entre componentes
    Navegación entre componentes (poder acceder a los componentes con el router) y funcionan los botones de redirección

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,30 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ProductMainComponent } from './productos/product-main/product-main.component';
+import { ProductListComponent } from './productos/product-main/product-list/product-list.component';
+import { ProductEditComponent } from './productos/product-main/product-edit/product-edit.component';
+import { ProductDetailComponent } from './productos/product-main/product-detail/product-detail.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+	{path: '', redirectTo: '/home', pathMatch: 'full'},
+	{path: 'home', component: HomeComponent},
+	{path: 'product', component: ProductMainComponent, children: [
+		{path: '', component: ProductListComponent, pathMatch: 'full'},
+		{path: 'new', component: ProductEditComponent},
+		{path: ':id', component: ProductDetailComponent},
+		{path: ':id/edit', component: ProductEditComponent}
+	]},
+	{path: 'monitoreo', component: ProductMainComponent, children: [
+		{path: '', component: ProductListComponent, pathMatch: 'full'}
+	]},
+	{path: '**', component: NotFoundComponent}
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+	imports: [RouterModule.forRoot(routes)],
+	exports: [RouterModule]
 })
 export class AppRoutingModule { }
