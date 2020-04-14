6c06896948a7d6dcfa3546ffc5cd2116d027bd96
PaulinaArvizu <is707571@iteso.mx>
2020-04-13 19:43:36 -0500

    app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
+import { ProductsService } from './productos/products.service';
+import { Product } from './productos/Product';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'practicaIntegradoraAngular';
+	title = 'practicaIntegradoraAngular';
+	
+	constructor() { }
 }
